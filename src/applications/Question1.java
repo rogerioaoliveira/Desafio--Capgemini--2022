099e0e94f47c1093e6d31601d5b4587c910ee1c1
Rogério A. Oliveira <rogerioalexandredeoliveira87@gmail.com>
2022-02-15 10:02:16 -0300

    First question resolved

---
+++ src/applications/Question1.java
@@ -0,0 +1,20 @@
+package applications;
+
+import java.util.Scanner;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter a number: ");
+		int n = sc.nextInt();
+		System.out.println();
+
+		for (int i = 0; i < n; i++) {
+			int aux = n - i;
+			System.out.println(" ".repeat(aux - 1) + "*".repeat(i + 1));
+		}
+	}
+}
